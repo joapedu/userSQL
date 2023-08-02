3cc21c42b2c2a80ec19afc92afca342e50ad7758
joapedu <joaoeduardobraga2@gmail.com>
2023-08-02 01:14:40 -0300

    feat: item 4 final

--- item2.4.sql
+++ item2.4.sql
@@ -1,1 +1,6 @@
 -- Consulta que retorne o valor total dos pedidos agrupados por nome de vendedor
+SELECT A3_NOME AS NOME_VENDEDOR, SUM(C6_QTD * C6_PRUNIT) AS VALOR_TOTAL
+FROM C5 - Cadastro de Pedidos
+INNER JOIN C6 - Itens do Pedido ON C5_NUM = C6_NUM
+INNER JOIN A3 - Cadastro de Vendedor ON C5_CODVEN = A3_COD
+GROUP BY A3_NOME;
