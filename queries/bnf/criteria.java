2eb92ae00db164215126bb7d26ed6ac8e2094749
joapedu <joaoeduardobraga2@gmail.com>
2023-10-16 11:56:04 -0300

    :rocket: feat: new criteria api

--- queries/bnf/criteria.java
+++ queries/bnf/criteria.java
@@ -14,7 +14,43 @@ public class Pet {
   protected String color;
   @ManyToOne
   protected Set<Owner> owners;
-  ...
+}
+
+@ManyToMany
+@JoinTable(
+    name="EJB_ROSTER_TEAM_PLAYER",
+    joinColumns=
+        @JoinColumn(name="TEAM_ID", referencedColumnName="ID"),
+    inverseJoinColumns=
+        @JoinColumn(name="PLAYER_ID", referencedColumnName="ID")
+)
+public Collection<Player> getPlayers() {
+    return players;
+}
+
+@ManyToMany(mappedBy="players")
+public Collection<Team> getTeams() {
+    return teams;
+}
+
+
+@Entity
+@Table(name = "EJB_ROSTER_LEAGUE")
+public abstract class League implements java.io.Serializable {
+    CriteriaQuery<Pet> cq = cb.createQuery(Pet.class);
+    Root<Pet> pet = cq.from(Pet.class);
+    cq.where(pet.get("color").in("brown", "black"));
+
+    CriteriaQuery<Pet> cq = cb.createQuery(Pet.class);
+    Root<Pet> pet = cq.from(Pet.class);
+    cq.select(pet);
+    cq.orderBy(cb.desc(pet.get("birthday")));
+
+    CriteriaQuery<Pet> cq = cb.createQuery(Pet.class);
+    Root<Pet> pet = cq.from(Pet.class);
+    cq.where(cb.equal(pet.get("name"), "Fido"));
+    TypedQuery<Pet> q = em.createQuery(cq);
+    List<Pet> results = q.getResultList();
 }
 
 @Static Metamodel(Pet.class)
@@ -25,3 +61,121 @@ public class Pet_ {
   public static volatile SingularAttribute<Pet, String> color;
   public static volatile SetAttribute<Pet, Owner> owners;
 }
+
+@Entity
+public class SummerLeague extends League
+         implements java.io.Serializable {
+
+    /** Creates a new instance of SummerLeague */
+    public SummerLeague() {
+    }
+
+    public SummerLeague(String id, String name,
+             String sport) throws IncorrectSportException {
+        this.id = id;
+        this.name = name;
+        if (sport.equalsIgnoreCase("swimming") ||
+                sport.equalsIgnoreCase("soccer") ||
+                sport.equalsIgnoreCase("basketball") ||
+                sport.equalsIgnoreCase("baseball")) {
+            this.sport = sport;
+        } else {
+            throw new IncorrectSportException(
+                "Sport is not a summer sport.");
+        }
+    }
+}
+
+@Entity
+@DiscriminatorColumn(name="DISCRIMINATOR")
+@Table(name="EJB_ROSTER_LEAGUE")
+
+public abstract class League implements java.io.Serializable {
+    CriteriaQuery<Pet> cq = cb.createQuery(Pet.class);
+    Metamodel m = em.getMetamodel();
+    EntityType<Pet> Pet_ = m.entity(Pet.class);
+    Root<Pet> pet = cq.from(Pet.class);
+    cq.where(cb.equal(pet.get(Pet_.name), "Fido"));
+
+    CriteriaQuery<Pet> cq = cb.createQuery(Pet.class);
+    Root<Pet> pet = cq.from(Pet.class);
+    Join<Owner, Address> address = pet.join("owners").join("addresses");
+}
+
+public List<PlayerDetails> getPlayersByPosition(String position) {
+    logger.info("getPlayersByPosition");
+    List<Player> players = null;
+    
+    try {
+        CriteriaQuery<Player> cq = cb.createQuery(Player.class);
+        if (cq != null) {
+            Root<Player> player = cq.from(Player.class);
+
+            // set the where clause
+            cq.where(cb.equal(player.get(Player_.position), position));
+            cq.select(player);
+            TypedQuery<Player> q = em.createQuery(cq);
+            players = q.getResultList();
+        }
+
+        return copyPlayersToDetails(players);
+    } catch (Exception ex) {
+        throw new EJBException(ex);
+    }
+}
+
+public List<PlayerDetails> getPlayersByPosition(String position) {
+    logger.info("getPlayersByPosition");
+    List<Player> players = null;
+    
+    try {
+        CriteriaQuery<Player> cq = cb.createQuery(Player.class);
+        if (cq != null) {
+            Root<Player> player = cq.from(Player.class);
+
+            // set the where clause
+            cq.where(cb.equal(player.get(Player_.position), position));
+            cq.select(player);
+            TypedQuery<Player> q = em.createQuery(cq);
+            players = q.getResultList();
+        }
+
+        return copyPlayersToDetails(players);
+    } catch (Exception ex) {
+        throw new EJBException(ex);
+    }
+}
+
+@Entity
+public class Contact implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @NotNull
+    protected String firstName;
+    @NotNull
+    protected String lastName;
+    @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+        +"[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*"
+        +"@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+             message="{invalid.email}")
+    protected String email;
+    @Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$",
+             message="{invalid.phonenumber}")
+    protected String mobilePhone;
+    @Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$",
+             message="{invalid.phonenumber}")
+    protected String homePhone;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    @Past
+    protected Date birthday;
+}
+
+@Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$",
+             message="{invalid.phonenumber}")
+    protected String homePhone;
+
+@Pattern(regexp="^\\(?(\\d{3})\\)?[- ]?(\\d{3})[- ]?(\\d{4})$",
+             message="Invalid phone number!")
+    protected String homePhone;
